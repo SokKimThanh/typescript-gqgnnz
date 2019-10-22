876ff11de1e95f02567473df012adecb1e6b5c77
SokKimThanh <thanhsk1991@gmail.com>
2019-10-22 19:34:53 +0700

    1 file added

---
+++ tuple.ts
@@ -0,0 +1,4 @@
+type ColorType = [string, number, number, number];
+let red: ColorType = ['red', 1, 0, 0];
+let green: [string, number, number, number] = ['Green', 0, 1, 0];
+let blue = ['Blue', 0, 0, 1]
