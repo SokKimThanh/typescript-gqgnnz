7ff31d6ab8f7ec719fb8e9cb4e71b0ccc9445315
SokKimThanh <thanhsk1991@gmail.com>
2019-10-25 06:35:04 +0700

    1 file modified

--- student-greeter-interface.ts
+++ student-greeter-interface.ts
@@ -1,18 +1,22 @@
-export {};
+export { };
 class Students {
-  fullName: string;
-  constructor(private person: Person) {
-    this.fullName =
-      person.firstName + " " + person.middleInitial + " " + person.lastName;
-  }
-  greeter() {
-    return "Hello, " + this.person.firstName + " " + this.person.lastName;
-  }
+    fullName: string;
+    constructor(private person: Person) {
+        this.fullName = person.firstName + " " + person.middleInitial + " " + person.lastName;
+
+    }
+    greeter() {
+        if (this.person.middleInitial) {
+            return "Hello, " + this.fullName;
+        } else {
+            return "Hello, " + this.person.firstName + " " + this.person.lastName;
+        }
+    }
 }
 interface Person {
-  firstName: string;
-  lastName: string;
-  middleInitial?: string;
+    firstName: string,
+    lastName: string,
+    middleInitial?: string,
 }
 
 let user = { firstName: "Thanh", lastName: "Sok" };
