6ab88fd090efe8ed5e3a4531bfbc75da1e5cccb2
SokKimThanh <thanhsk1991@gmail.com>
2019-10-30 10:19:52 +0700

    1 file modified

--- IIFE-expresion-var-keyword.ts
+++ IIFE-expresion-var-keyword.ts
@@ -1,22 +1,28 @@
-var waiting_time_out_side = function(print_number_in_side: Function, milisecond: number) {
-    // scope argument milisecond print_number
-    setTimeout(print_number_in_side(milisecond), milisecond * 100);
-}
+export {};
+var waiting_time_out_side = function(
+  print_number_in_side: Function,
+  milisecond: number
+) {
+  // scope argument milisecond print_number
+  setTimeout(print_number_in_side(milisecond), milisecond * 100);
+};
 // Ensure
-var print_number = function (i: number): void {
-    if (i) {
-        console.log(i);
-    }
-}
+var print_number = function(i: number): void {
+  if (i) {
+    console.log(i);
+  }
+};
 for (var i = 0; i < 10; i++) {
-    // scope var i
-    waiting_time_out_side((print_number), i);
+  // scope var i
+  waiting_time_out_side(print_number, i);
 }
 // IIFE expression
 for (var i = 0; i < 10; i++) {
-    // capture the current state of 'i'
-    // by invoking a function with its current value 
-    (function(i) {
-        setTimeout(function() { console.log(i); }, 100 * i);
-    })(i);
+  // capture the current state of 'i'
+  // by invoking a function with its current value
+  (function(i) {
+    setTimeout(function() {
+      console.log(i);
+    }, 100 * i);
+  })(i);
 }
